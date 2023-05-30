71c5774eba2fcb3347170dacea0f489af0aa20dc
pmilowski <pmilowski16@gmail.com>
2023-05-30 13:44:34 +0200

    Sparks

--- Assets/Script/ActivateHammer.cs
+++ Assets/Script/ActivateHammer.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class ActivateHammer : MonoBehaviour
 {
     public HingeJoint HammerHolder;
+    public GameObject Sparks;
+
+    private void Start()
+    {
+        Sparks.SetActive(false);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag.Equals("HammerActivator"))
         {
+            Sparks.SetActive(true);
             // Debug.Log("triggered!");
             var limits = HammerHolder.limits;
             limits.max = 90f;
