54c2b91216f95298c1f52a3a3d821c025f5dbee6
pmilowski <pmilowski16@gmail.com>
2023-06-05 10:00:02 +0200

    ball push button on car

--- Assets/Script/ActivateCar.cs
+++ Assets/Script/ActivateCar.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class ActivateCar : MonoBehaviour
 {
     public ConstantForce constantForce;
+    public Rigidbody rb;
     public float velocity = 5f;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("CarActivator"))
         {
-            Debug.Log("Engine start!");
+            // Debug.Log("Engine start!");
             var force = constantForce.relativeForce;
             force.z = velocity;
             constantForce.relativeForce = force;
+            rb.constraints = RigidbodyConstraints.FreezePosition;
         }
     }
 }
